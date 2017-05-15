f62c994e2b95795393644a5c8dd6e89fc0b85961
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-05-15 10:33:22 -0300

    up

--- PersonDetector.cpp
+++ PersonDetector.cpp
@@ -144,8 +144,8 @@ void PersonDetector::getFrameAndDetect(Mat &frame)
     if(persons.size()==0){
         detectFaceAllSizes(resizedFrame); // Detect using cascades over whole image
     }else{
-        // detectFacesGoodFeaturesToTrack(resizedFrame); // Detect using template matching
-        detectFacesTemplateMatching(resizedFrame);
+        // detectFacesGoodFeaturesToTrack(resizedFrame); 
+        detectFacesTemplateMatching(resizedFrame);// Detect using template matching
     }
 }
 
